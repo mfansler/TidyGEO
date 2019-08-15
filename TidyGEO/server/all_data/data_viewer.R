fdd654a0da06368198518c0ec9be29a6610a7b3b
Avery Bell <avery.a.bell@byu.net>
2019-08-15 14:06:18 -0600

    Enabled column navigation for "View datasets" (in "all data")

--- TidyGEO/server/all_data/data_viewer.R
+++ TidyGEO/server/all_data/data_viewer.R
@@ -2,6 +2,25 @@ data_to_view_is_null <- reactive({
   eval(parse(text = paste0("is.null(", input$data_to_view, "_vals$", input$data_to_view, "_data)")))
 })
 
+observe({
+  all_vals$use_viewing_subset <- !is.null(ncol(all_vals$all_data)) && ncol(all_vals$all_data) > 19
+  all_vals$viewing_subset <- c(all_vals$viewing_min, min(all_vals$viewing_min + 5, ncol(all_vals$all_data)))
+}, priority = 1)
+
+all_in_view <- reactive({
+  if (all_vals$use_viewing_subset) {
+    all_vals$all_data[all_vals$viewing_subset[1]:all_vals$viewing_subset[2]]
+  } else {
+    all_vals$all_data
+  }
+})
+
+output$all_vals_viewing_subset <- renderUI({
+  if (eval(expr(`$`(!!sym(paste0(input$data_to_view, "_vals")), "use_viewing_subset")))) {
+    col_navigation_set(input$data_to_view, "data_viewer")
+  }
+})
+
 output$view_data <- renderUI({
   if (!is.null(input$data_to_view)) {
     if (data_to_view_is_null()) {
@@ -28,17 +47,17 @@ dt_opts <- list(scrollX = TRUE,
                 )))
 
 output$clinical_view <- renderDT({
-  clinical_vals$clinical_data
+  clinical_in_view()
 }, options = dt_opts)
 
 output$assay_view <- renderDT({
-  assay_vals$assay_data
+  assay_in_view()
 }, options = dt_opts)
 
 output$feature_view <- renderDT({
-  feature_vals$feature_data
+  feature_in_view()
 }, options = dt_opts)
 
 output$all_view <- renderDT({
-  all_vals$all_data
+  all_in_view()
 }, options = dt_opts)
--- TidyGEO/ui/all_data/data_viewer.R
+++ TidyGEO/ui/all_data/data_viewer.R
@@ -1,5 +1,8 @@
 tabPanel(title = "View datasets", value = "2",
   br(),
   selectInput("data_to_view", "Please select the dataset you would like to view:", choices = c("clinical", "assay", "feature", "all")),
+  br(),
+  uiOutput("all_vals_viewing_subset"),
+  br(),
   uiOutput("view_data")
 )
