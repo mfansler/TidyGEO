bcd6ce5e301f521b3a47e534692b1d877d3e63ff
biqe2 <77342463+biqe2@users.noreply.github.com>
2021-12-02 12:11:28 -0700

    Update 7_filter_vals.R
    updated wording

--- TidyGEO/ui/clinical/7_filter_vals.R
+++ TidyGEO/ui/clinical/7_filter_vals.R
@@ -1,5 +1,5 @@
 tabPanel(title = EXCLUDE_ICON, value = "7",
-         h4("Filtering samples"),
+         h4("Filter samples"),
          p("You may want to remove some of the samples in the data, for example, if you have samples with missing (NA) values."), 
          p("Here, you can specify values to use as filtering criteria.
            Rows that match the filtering criteria for the specified column will be removed."),
