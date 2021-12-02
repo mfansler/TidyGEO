b456db24841564029ce737e205139c11e055296f
biqe2 <77342463+biqe2@users.noreply.github.com>
2021-12-02 12:08:41 -0700

    Update 2_shift_cells.R
    updating wording

--- TidyGEO/ui/clinical/2_shift_cells.R
+++ TidyGEO/ui/clinical/2_shift_cells.R
@@ -1,22 +1,21 @@
 tabPanel(title = SHIFT_ICON, value = "2",
          h4("Extract shifted values"),
-         p('Sometimes, missing values can cause data from a single variable (such as "Sex" and "Freq" in ', 
+         p('Sometimes, missing values can cause data to be scattered across multiple columns (such as "Sex" and "Freq" in ', 
            actionLink(inputId = "show_broken_cols_example", label = "this example"),
-           ') to be scattered across multiple columns. This makes it difficult to extract data for the single variable.'),
-         p("Here, you can identify columns whose values are in the incorrect columns, choose a common pattern, and organize it 
-           according to it." 
+           '). This makes it difficult to analyze the data.'),
+         p("Here, you can identify columns that have scattered values, specify a pattern that matches the values you want to extract, and extract those values to a new column." 
            ),
            
 
    #based on num 3 split pairs        
-         tags$b("Please select columns that contain key-value pairs:"),
+         tags$b("Please select columns that contain values that you wish to extract:"),
                                                             #help_link("clinical", "split_help")),
 
                           checkboxInput(inputId = "select_all_shift", 
                                         label = tags$i("Select all")),
                           uiOutput("choose_cols_to_shift"),
                           textInput(inputId = "shift_pattern", label = "Pattern (including any spaces): ", placeholder = "Start typing..."),
-                          textInput(inputId = "new_col_name", label = "New Column name:", placeholder = "Start typing...")
+                          textInput(inputId = "new_col_name", label = "New column name:", placeholder = "Start typing...")
          #)
          ,
          div(p(
